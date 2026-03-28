ed59a90eac5bbbbd85c19d6d510e1a89959c984a
lahiuseuok <lahirusandeepa22@gmail.com>
2026-03-28 10:53:58 +0530

    misssing env

--- src/supabase.js
+++ src/supabase.js
@@ -1,12 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabaseUrl = 'https://rqmwtlzvizwpsutklpsp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxbXd0bHp2aXp3cHN1dGtscHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2NjA5MTQsImV4cCI6MjA5MDIzNjkxNH0.kHZKxr4ubGK5Hz6pN81D_Wgxc4tTFRKMwhnr1LHbe - g';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
-
-
-
-
+if (!supabaseUrl || !supabaseKey) {
+    console.error("Missing Supabase ENV")
+}
