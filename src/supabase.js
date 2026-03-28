8f1c669223d64c16a44dbff63d8327de61b1c72c
lahiuseuok <lahirusandeepa22@gmail.com>
2026-03-28 10:40:42 +0530

    supabase js edit

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://rqmwtlzvizwpsutklpsp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxbXd0bHp2aXp3cHN1dGtscHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2NjA5MTQsImV4cCI6MjA5MDIzNjkxNH0.kHZKxr4ubGK5Hz6pN81D_Wgxc4tTFRKMwhnr1LHbe - g';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+
+
+
+
