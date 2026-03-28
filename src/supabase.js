e92c55bb561119968858271eba1d57525a1b1527
lahiuseuok <lahirusandeepa22@gmail.com>
2026-03-28 11:05:53 +0530

    no commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,16 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://rqmwtlzvizwpsutklpsp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxbXd0bHp2aXp3cHN1dGtscHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2NjA5MTQsImV4cCI6MjA5MDIzNjkxNH0.kHZKxr4ubGK5Hz6pN81D_Wgxc4tTFRKMwhnr1LHbe - g';
+const supabaseAnonKey = 'YOUR_REAL_KEY_HERE'; // remove "- g"
 
-if (!supabaseUrl || !supabaseKey) {
-    console.error("Missing Supabase ENV")
+if (!supabaseUrl || !supabaseAnonKey) {
+    console.error("Missing Supabase config");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
-
-
-
-
